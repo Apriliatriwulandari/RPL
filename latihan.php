3f4a9218b0c525eafd766c60cfdc77e487640025
Aprilia Tri Wulandari <63582176+Apriliatriwulandari@users.noreply.github.com>
2020-08-27 08:39:57 +0700

    Add files via upload

--- latihan.php
+++ latihan.php
@@ -1,7 +1,21 @@
-<?php 
-class Baju{
-}
-	$baju1 = new Baju();
-	$baju2 = new Baju();
-	$baju3 = new Baju();
- ?>
+<?PHP
+    class baju{
+        public $merkbaju = "cardinal",
+                $ukuran = "xl",
+                    $harga = 100000;
+        public function getmerkbaju(){
+            return "Hai ini baju saya";
+            }
+        public function getukuran(){
+             return "ukurannya adalah xl";
+            }
+    }
+
+    $baju1 = new baju();
+
+    echo $baju1->getmerkbaju();
+
+    echo "<br>";
+
+    echo $baju1->getukuran();
+?>
